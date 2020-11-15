c9e3ab9bc0b18fb3e37443cf9e9253b017d7f8bf
halfnelson <dpershouse@gmail.com>
2020-11-15 11:39:38 +1000

    reduce sample scope for more concentrated tests

---
+++ test/compat-samples/generate.ts
@@ -0,0 +1,19 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { parse } from 'svelte-parse';
+
+
+const fixtures =__dirname;
+
+const inputs = fs
+	.readdirSync(fixtures, { encoding: 'utf-8', withFileTypes: true })
+	.filter(
+		f => f.isDirectory()
+	)
+    .map((f) => f.name)
+
+inputs.forEach(sample_folder => {
+    const input = fs.readFileSync(path.join(fixtures, sample_folder, 'input.svelte')).toString().replace(/\r\n/gi, "\n");
+    const expected = parse({value: input, generatePositions: true});
+    fs.writeFileSync(path.join(fixtures, sample_folder, 'output.json'), JSON.stringify(expected, null, 2));
+})
--- test/compat.test.ts
+++ test/compat.test.ts
@@ -1,1 +1,31 @@
-import './compat/samples.test'
+import * as path from 'path';
+import * as fs from 'fs';
+
+import { parse } from '../src/index'; //Modified to suit htmlx-parser-svast
+
+const fixtures = path.join(__dirname, 'compat-samples');
+
+const inputs = fs
+	.readdirSync(fixtures, { encoding: 'utf-8', withFileTypes: true })
+	.filter(
+		f => f.isDirectory()
+	)
+	.map((f) => f.name)
+
+
+import { suite } from 'uvu';
+import * as assert from 'uvu/assert';
+
+const samples = suite('compat-sample');
+
+inputs.forEach(sample_folder => {
+	samples(`inputs should equal outputs: ${sample_folder}`, () => {
+        const input = fs.readFileSync(path.join(fixtures, sample_folder, 'input.svelte')).toString().replace(/\r\n/gi, "\n");
+        const expected = JSON.parse(
+			fs.readFileSync(path.join(fixtures, sample_folder, 'output.json')).toString()
+		)
+		assert.equal(parse(input).ast, expected);
+	});
+});
+
+samples.run();
